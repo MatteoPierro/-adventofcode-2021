09209fcbb9a7013f5d6326590143296163cfab08
Matteo Pierro <pierromatteo@gmail.com>
2021-11-29 17:40:09 +0200

    initial commit

---
+++ advent_of_code/day1.py
@@ -0,0 +1,3 @@
+class Day1:
+    def foo(self):
+        return False
---
+++ tests/test_day1.py
@@ -0,0 +1,12 @@
+import unittest
+
+from advent_of_code.day1 import Day1
+
+
+class Day1Test(unittest.TestCase):
+    def test_foo(self):
+        self.assertTrue(Day1().foo())
+
+
+if __name__ == '__main__':
+    unittest.main()
