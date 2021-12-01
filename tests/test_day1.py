11acb8978db7f8f4e1df82cfdac6b4bfc4cf0237
Matteo Pierro <pierromatteo@gmail.com>
2021-12-01 18:41:33 +0200

    solve first puzzle day1

--- tests/test_day1.py
+++ tests/test_day1.py
@@ -1,11 +1,20 @@
 import unittest
 
-from advent_of_code.day1 import Day1
+from advent_of_code.utilities import read_lines
+
+
+def increased_depth_measurements(depth_measurements):
+    return len(list(filter(lambda t: t[1] >= t[0], zip(depth_measurements, depth_measurements[1:]))))
 
 
 class Day1Test(unittest.TestCase):
-    def test_foo(self):
-        self.assertTrue(Day1().foo())
+    def test_increased_depth_measurements(self):
+        depth_measurements = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263]
+        self.assertEqual(7, increased_depth_measurements(depth_measurements))
+
+    def test_day1(self):
+        depth_measurements = list(map(int, read_lines('input_day1.txt')))
+        self.assertEqual(1616, increased_depth_measurements(depth_measurements))
 
 
 if __name__ == '__main__':
