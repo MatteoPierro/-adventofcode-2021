2cb6aa10cbef7b7a434962c040ae2dc0d66eaba7
Matteo Pierro <pierromatteo@gmail.com>
2021-12-05 15:09:43 +0200

    solve second puzzle day 5

--- tests/test_hydrothermal_venture.py
+++ tests/test_hydrothermal_venture.py
@@ -10,15 +10,28 @@ class Line:
         self.end = end
 
     def all_points(self):
-        if self.is_vertical():
-            start = min(self.start[1], self.end[1])
-            end = max(self.start[1], self.end[1])
-            return [(self.start[0], i) for i in range(start, end + 1)]
-        if self.is_horizontal():
-            start = min(self.start[0], self.end[0])
-            end = max(self.start[0], self.end[0])
-            return [(i, self.start[1]) for i in range(start, end + 1)]
-        raise 'Not implemented yet!'
+        min_x_point = min(self.start, self.end)
+        max_x_point = max(self.start, self.end)
+        x_increment = self.calculate_x_increment()
+        y_increment = self.calculate_y_increment()
+        points = [min_x_point]
+        while points[-1] != max_x_point:
+            last_point = points[-1]
+            points.append((last_point[0] + x_increment, last_point[1] + y_increment))
+        return points
+
+    def calculate_x_increment(self):
+        return 0 if self.is_vertical() else 1
+
+    def calculate_y_increment(self):
+        min_x_point = min(self.start, self.end)
+        max_x_point = max(self.start, self.end)
+        if min_x_point[1] > max_x_point[1]:
+            return -1
+        elif min_x_point[1] < max_x_point[1]:
+            return 1
+        else:
+            return 0
 
     def is_vertical_or_horizontal(self):
         return self.is_vertical() or self.is_horizontal()
@@ -45,6 +58,12 @@ def number_of_points_with_two_or_more_occurrences(lines):
     return len([elem for elem in point_occurrences.items() if elem[1] > 1])
 
 
+def number_of_all_points_with_two_or_more_occurrences(lines):
+    points = [point for line in lines for point in line.all_points()]
+    point_occurrences = collections.Counter(points)
+    return len([elem for elem in point_occurrences.items() if elem[1] > 1])
+
+
 def horizontal_and_vertical_lines(lines):
     return [line for line in lines if line.is_vertical_or_horizontal()]
 
@@ -63,6 +82,8 @@ class HydrothermalVentureTest(unittest.TestCase):
     def test_points_in_line(self):
         self.assertEqual([(0, 9), (1, 9), (2, 9), (3, 9), (4, 9), (5, 9)], Line((0, 9), (5, 9)).all_points())
         self.assertEqual([(7, 0), (7, 1), (7, 2), (7, 3), (7, 4)], Line((7, 4), (7, 0)).all_points())
+        self.assertEqual([(1, 1), (2, 2), (3, 3)], Line((1, 1), (3, 3)).all_points())
+        self.assertEqual([(7, 9), (8, 8), (9, 7)], Line((9, 7), (7, 9)).all_points())
 
     def test_filter_horizontal_and_vertical_lines(self):
         lines = [Line((0, 9), (5, 9)),
@@ -78,6 +99,7 @@ class HydrothermalVentureTest(unittest.TestCase):
         self.assertEqual([Line((0, 9), (5, 9)), Line((9, 4), (3, 4)), Line((2, 2), (2, 1)), Line((7, 0), (7, 4)),
                           Line((0, 9), (2, 9)), Line((3, 4), (1, 4))], horizontal_and_vertical_lines(lines))
         self.assertEqual(5, number_of_points_with_two_or_more_occurrences(lines))
+        self.assertEqual(12, number_of_all_points_with_two_or_more_occurrences(lines))
 
     def test_parse_line(self):
         self.assertEqual(Line((2, 2), (2, 1)), parse_line('2,2 -> 2,1'))
@@ -86,3 +108,8 @@ class HydrothermalVentureTest(unittest.TestCase):
         raw_lines = read_lines('input_day5.txt')
         lines = [parse_line(line) for line in raw_lines]
         self.assertEqual(7085, number_of_points_with_two_or_more_occurrences(lines))
+
+    def test_puzzle2(self):
+        raw_lines = read_lines('input_day5.txt')
+        lines = [parse_line(line) for line in raw_lines]
+        self.assertEqual(20271, number_of_all_points_with_two_or_more_occurrences(lines))
