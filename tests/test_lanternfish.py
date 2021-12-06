71fd71b94692781ecbda5f23d7b43979cdb63515
Matteo Pierro <pierromatteo@gmail.com>
2021-12-06 19:07:01 +0200

    solve second puzzle day 6

--- tests/test_lanternfish.py
+++ tests/test_lanternfish.py
@@ -1,29 +1,36 @@
 import unittest
+from collections import Counter
 
 from advent_of_code.utilities import read_lines
 
 
-def evolve(current_population, iterations=1):
-    if iterations == 0:
-        return current_population
-    new_population = []
-    new_born = []
-    for fish_timer in current_population:
-        if fish_timer == 0:
-            new_born.append(8)
-            new_population.append(6)
-        else:
-            new_population.append(fish_timer - 1)
-    return evolve(new_population + new_born, iterations - 1)
+def evolve(initial_population, iterations=1):
+    timers_occurrences = Counter(initial_population)
+    for _ in range(iterations):
+        first_timer = timers_occurrences[0]
+        for timer_index in range(8):
+            timers_occurrences[timer_index] = timers_occurrences[timer_index + 1]
+        timers_occurrences[6] += first_timer
+        timers_occurrences[8] = first_timer
+    return timers_occurrences
+
+
+def population_size_after_evolution(initial_population, iterations):
+    return sum(evolve(initial_population, iterations).values())
+
+
+def read_initial_population():
+    return list(map(int, read_lines('input_day6.txt')[0].split(',')))
 
 
 class LanternfishTest(unittest.TestCase):
     def test_evolve(self):
         initial_population = [3, 4, 3, 1, 2]
-        self.assertEqual([2, 3, 2, 0, 1], evolve(initial_population))
-        self.assertEqual([5, 6, 5, 3, 4, 5, 6, 7, 7, 8], evolve(initial_population, 5))
-        self.assertEqual(5934, len(evolve(initial_population, 80)))
+        self.assertEqual(5934, population_size_after_evolution(initial_population, 80))
+        self.assertEqual(26984457539, population_size_after_evolution(initial_population, 256))
 
     def test_puzzle1(self):
-        initial_population = list(map(int, read_lines('input_day6.txt')[0].split(',')))
-        self.assertEqual(374994, len(evolve(initial_population, 80)))
+        self.assertEqual(374994, population_size_after_evolution(read_initial_population(), 80))
+
+    def test_puzzle2(self):
+        self.assertEqual(1686252324092, sum(evolve(read_initial_population(), 256).values()))
