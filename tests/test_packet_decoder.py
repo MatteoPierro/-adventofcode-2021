d381b9413dd22730ad5b9e3ba521ec33933b7888
Matteo Pierro <pierromatteo@gmail.com>
2021-12-17 07:20:39 +0100

    wip

--- tests/test_packet_decoder.py
+++ tests/test_packet_decoder.py
@@ -29,7 +29,7 @@ def convert_hex_string(hex_string):
     return binary_string
 
 
-def read_headers(binary_packets, headers, index=0):
+def read_packet_versions(binary_packets, headers, index=0):
     if len(binary_packets) == 0:
         return headers
     if all(v == '0' for v in binary_packets):
@@ -44,20 +44,108 @@ def read_headers(binary_packets, headers, index=0):
         while binary_packets[index] != '0':
             index += 5
         index += 5
-        return read_headers(binary_packets[index:], headers)
+        return read_packet_versions(binary_packets[index:], headers)
 
     length_type_id = int(binary_packets[index], 2)
     index += 1
     if length_type_id == 0:
         index += 15
-        return read_headers(binary_packets[index:], headers)
+        return read_packet_versions(binary_packets[index:], headers)
     index += 11
-    return read_headers(binary_packets[index:], headers)
+    return read_packet_versions(binary_packets[index:], headers)
+
+
+def parse_packet(packet):
+    expression_type = int(packet[3:6], 2)
+    packet = packet[6:]
+    if expression_type == 4:
+        number, packet = read_number(packet)
+        return Number(number), packet
+    if expression_type == 0:
+        number_of_packets, packet = read_number_of_packets(packet)
+        operands, packet = read_operands(number_of_packets, packet)
+        return Sum(operands), packet
+    if expression_type == 1:
+        number_of_packets, packet = read_number_of_packets(packet)
+        print(number_of_packets)
+        operands, packet = read_operands(number_of_packets, packet)
+        return Product(operands), packet
+
+
+def read_operands(number_of_packets, packet):
+    operands = []
+    for _ in range(number_of_packets):
+        (n, packet) = parse_packet(packet)
+        operands.append(n)
+    return operands, packet
+
+
+def read_number(packet):
+    number = ''
+    while packet[0] != '0':
+        number += str(int(packet[:  5], 2))
+        packet = packet[5:]
+    number += str(int(packet[:  5], 2))
+    return int(number), packet[5:]
+
+
+def read_number_of_packets(packet):
+    length_type = packet[0]
+    packet = packet[1:]
+    if length_type == '0':
+        print(packet[: 15])
+        number_of_packets = int(packet[: 15], 2)
+        packet = packet[15:]
+    else:
+        number_of_packets = int(packet[: 11], 2)
+        packet = packet[11:]
+    return number_of_packets, packet
 
 
 class PacketDecoderTest(unittest.TestCase):
     def test_puzzle1(self):
         raw_packets = read_lines('input_day16.txt')[0]
-        headers = []
-        read_headers(convert_hex_string(raw_packets), headers)
-        self.assertEqual(974, sum(headers))
+        self.assertEqual(974, sum(read_packet_versions(convert_hex_string(raw_packets), [])))
+
+    def test_sum_packet(self):
+        packet = convert_hex_string('C200B40A82')
+        (operation, _) = parse_packet(packet)
+        self.assertEqual(3, operation.calculate())
+
+    def test_product_packet(self):
+        packet = convert_hex_string('04005AC33890')
+        print(packet)
+        (operation, _) = parse_packet(packet)
+        self.assertEqual(3, operation.calculate())
+
+
+class Number:
+    def __init__(self, value):
+        self.value = value
+
+    def calculate(self):
+        return self.value
+
+    def __str__(self):
+        return f"{self.calculate()}"
+
+
+class Sum:
+    def __init__(self, operands):
+        self.operands = operands
+
+    def calculate(self):
+        value = 0
+        for op in self.operands:
+            value += op.calculate()
+        return value
+
+class Product:
+    def __init__(self, operands):
+        self.operands = operands
+
+    def calculate(self):
+        value = 0
+        for op in self.operands:
+            value *= op.calculate()
+        return value
