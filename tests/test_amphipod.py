f6f2c31f9d76978f66e75cbc6cc830a7348f186d
Matteo Pierro <pierromatteo@gmail.com>
2021-12-24 00:14:37 +0100

    try to solve second puzzle day23

--- tests/test_amphipod.py
+++ tests/test_amphipod.py
@@ -32,9 +32,6 @@ def calculate_path(a, b):
     return path
 
 
-world = [(x, 0) for x in range(11)]
-world += [(2, 1), (2, 2), (4, 1), (4, 2), (6, 1), (6, 2), (8, 1), (8, 2)]
-
 end_positions = {
     ((2, 1), 'A'),
     ((2, 2), 'A'),
@@ -54,10 +51,6 @@ bonus = {
 
 
 class AmphipodTest(unittest.TestCase):
-    def test_bug(self):
-        path = calculate_path((4, 1), (6, 1))
-        print(path)
-
     def test_distance(self):
         self.assertEqual(3, distance((0, 0), (2, 1)))
         self.assertEqual(6, distance((1, 0), (6, 1)))
@@ -67,7 +60,6 @@ class AmphipodTest(unittest.TestCase):
         amphipod_positions = {((4, 1), 'C'), ((3, 0), 'A'), ((5, 0), 'B'), ((2, 1), 'B'), ((9, 0), 'D'), ((4, 2), 'D'),
                               ((6, 2), 'C'), ((2, 2), 'A')}
         path = calculate_path((4, 1), (6, 1))
-        print(path)
         self.assertTrue(any(p in path for (p, _) in amphipod_positions))
 
     def test_calculate_path(self):
@@ -111,15 +103,6 @@ class AmphipodTest(unittest.TestCase):
 
     def test_puzzle1(self):
         queue = PriorityQueue()
-        # amphipod_positions = {
-        #     ((4, 1), 'A'),
-        #     ((2, 2), 'A'),
-        #     ((2, 1), 'B'),
-        #     ((6, 2), 'C'),
-        #     ((4, 2), 'B'),
-        #     ((6, 1), 'C'),
-        #     ((8, 1), 'D'),
-        #     ((8, 2), 'D')}
         amphipod_positions = {
             ((2, 1), 'A'),
             ((2, 2), 'C'),
@@ -132,6 +115,96 @@ class AmphipodTest(unittest.TestCase):
         queue.put((0, amphipod_positions, []))
         cost, pp = best_cost(queue, set())
         self.assertEqual(15365, cost)
+
+    def test_best_cost_extended(self):
+        queue = PriorityQueue()
+        # amphipod_positions = {
+        #     ((4, 1), 'A'),
+        #     ((2, 2), 'A'),
+        #     ((2, 1), 'B'),
+        #     ((6, 2), 'C'),
+        #     ((4, 2), 'B'),
+        #     ((6, 1), 'C'),
+        #     ((8, 1), 'D'),
+        amphipod_positions = {
+            ((2, 1), 'B'),
+            ((2, 2), 'D'),
+            ((2, 3), 'D'),
+            ((2, 4), 'A'),
+            ((4, 1), 'C'),
+            ((4, 2), 'C'),
+            ((4, 3), 'B'),
+            ((4, 4), 'D'),
+            ((6, 1), 'B'),
+            ((6, 2), 'B'),
+            ((6, 3), 'A'),
+            ((6, 4), 'C'),
+            ((8, 1), 'D'),
+            ((8, 2), 'A'),
+            ((8, 3), 'C'),
+            ((8, 4), 'A')}
+        queue.put((0, amphipod_positions, []))
+        cost, pp = best_cost_extended(queue, set())
+        acc = amphipod_positions
+        for p in pp:
+            print(f"{acc - p} -> {p - acc}")
+            acc = p
+            print()
+        self.assertEqual(12521, cost)
+
+
+def best_cost_extended(queue, memo):
+    while not queue.empty():
+        current_cost, amphipod_positions, previous_paths = queue.get()
+        if amphipod_positions == extended_end_positions:
+            previous_paths.append(amphipod_positions)
+            return current_cost, previous_paths
+        memo.add(tuple(amphipod_positions))
+        for amphipod_position in amphipod_positions:
+            start, amphipod = amphipod_position
+            new_amphipod_positions = amphipod_positions.copy()
+            new_amphipod_positions.remove(amphipod_position)
+            ## already in the right position
+            if start[0] == DESTINATIONS_COLUMN[amphipod] and (
+                    all(((DESTINATIONS_COLUMN[amphipod], c), amphipod) in amphipod_positions for c in
+                        range(start[0], 5))):
+                destinations = []
+            else:
+                destinations = []
+                positions = [p for (p, _) in amphipod_positions]
+                if (DESTINATIONS_COLUMN[amphipod], 4) not in positions:
+                    destinations = [(DESTINATIONS_COLUMN[amphipod], 4)]
+                elif (DESTINATIONS_COLUMN[amphipod], 3) not in positions and (
+                        ((DESTINATIONS_COLUMN[amphipod], 4), amphipod)) in amphipod_positions:
+                    destinations = [(DESTINATIONS_COLUMN[amphipod], 3)]
+                elif (DESTINATIONS_COLUMN[amphipod], 2) not in positions and \
+                        all(((DESTINATIONS_COLUMN[amphipod], c), amphipod) in amphipod_positions for c in
+                            range(3, 5)):
+                    destinations = [(DESTINATIONS_COLUMN[amphipod], 2)]
+                elif (DESTINATIONS_COLUMN[amphipod], 1) not in positions and \
+                        all(((DESTINATIONS_COLUMN[amphipod], c), amphipod) in amphipod_positions for c in
+                            range(2, 5)):
+                    destinations = [(DESTINATIONS_COLUMN[amphipod], 1)]
+                if len(destinations) == 0 and start[1] != 0:
+                    destinations = AISLE
+            for destination in destinations:
+                if start == destination:
+                    continue
+                path = calculate_path(start, destination)
+                # print(path)
+                if any(p in path for (p, _) in amphipod_positions):
+                    # print('skip this solution')
+                    continue
+                else:
+                    positions = copy(new_amphipod_positions)
+                    positions.add((destination, amphipod))
+                    new_cost = current_cost + bonus[amphipod] * len(path)
+                    if tuple(positions) in memo:
+                        # print('here')
+                        continue
+                    pp = copy(previous_paths)
+                    pp.append(positions)
+                    queue.put((new_cost, positions, pp))
 
 
 def best_cost(queue, memo):
@@ -179,6 +252,24 @@ def best_cost(queue, memo):
                     pp.append(positions)
                     queue.put((new_cost, positions, pp))
 
+
+extended_end_positions = {
+    ((2, 1), 'A'),
+    ((2, 2), 'A'),
+    ((2, 3), 'A'),
+    ((2, 4), 'A'),
+    ((4, 1), 'B'),
+    ((4, 2), 'B'),
+    ((4, 3), 'B'),
+    ((4, 4), 'B'),
+    ((6, 1), 'C'),
+    ((6, 2), 'C'),
+    ((6, 3), 'C'),
+    ((6, 4), 'C'),
+    ((8, 1), 'D'),
+    ((8, 2), 'D'),
+    ((8, 3), 'D'),
+    ((8, 4), 'D')}
 
 AISLE = [(0, 0), (1, 0), (3, 0), (5, 0), (7, 0), (9, 0), (10, 0)]
 
